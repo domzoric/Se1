2a0cf1955a48d26f1adba871614d815e3f38c78c
Justus Braun <justusbraun03@gmail.com>
2024-04-15 21:52:42 +0200

    Factory is in Bussnis logic

--- src/main/java/org/hbrs/se1/ss24/uebung1/client/Client.java
+++ src/main/java/org/hbrs/se1/ss24/uebung1/client/Client.java
@@ -1,5 +1,7 @@
 package org.hbrs.se1.ss24.uebung1.client;
 
+import org.hbrs.se1.ss24.uebung1.businesslogic.NumberFactory;
+
 public class Client {
     public void printTransformation(int number) {
         String result = NumberFactory.transformNumber("ROMAN", number);
